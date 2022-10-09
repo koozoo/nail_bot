63c7e7c68469c8843b8a002c91ba8ea0d2d9925b
Pavel Komlevskiy <hekpacoff@gmail.com>
2022-10-09 22:40:24 +0300

    create file

---
+++ bot.py
@@ -0,0 +1,29 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN_CHANEL
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN_CHANEL)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler(commands=['buy'])
+async def echo(message: types.Message):
+
+    # old style:
+
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer('Покупка курса')
+
+
+def main():
+    executor.start_polling(dp, skip_updates=True)
